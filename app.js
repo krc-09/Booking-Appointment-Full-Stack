913527b3069c85e187842685ac9388c87be4c418
krc-09 <kankanarc2020@gmail.com>
2024-11-28 18:55:19 +0530

    Made it realtime

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/messages', messageRoutes);
 Users.hasMany(Messages);
 Messages.belongsTo(Users);
 
-sequelize.sync({force:true})
+sequelize.sync()
   .then(result => {
     console.log('Database synced');
     app.listen(3000, () => {
